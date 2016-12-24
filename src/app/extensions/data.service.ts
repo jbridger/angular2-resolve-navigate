46ddc1d66ca355b32e3695fe9eaa3e74ec08fa9d
Jonathan Bridger <BDFun@Jonathans-MacBook-Pro.local>
2016-12-24 22:06:28 +0000

    Error not reproduced

--- src/app/extensions/data.service.ts
+++ src/app/extensions/data.service.ts
@@ -10,7 +10,8 @@ export class DataService {
   public data: string = 'init';
 
   public getData(): Observable<ServiceResult> {
-    return Observable.throw(new ServiceResult(undefined, 'errored'));
+    // return Observable.throw(new ServiceResult(undefined, 'errored'));
+    return Observable.of(new ServiceResult('we are good', undefined));
   }
 
 }
